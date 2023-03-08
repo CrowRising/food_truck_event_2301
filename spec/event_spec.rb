dd5d473a3151a9358798a27936f0dd764a459d40
Crow Rising <miscbyrd@gmail.com>
2023-03-08 10:15:46 -0700

    test: Add test for food trucks that sell

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -38,19 +38,36 @@ RSpec.describe Event do
       @food_truck2.stock(@item3, 25)
       @food_truck3.stock(@item1, 65) 
 
-      @event.add_food_truck(@food_truck1)    
-      @event.add_food_truck(@food_truck2)    
+      @event.add_food_truck(@food_truck1)
+      @event.add_food_truck(@food_truck2)
       @event.add_food_truck(@food_truck3)
 
       expect(@event.food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
     end
 
     it 'has food truck names' do
-      @event.add_food_truck(@food_truck1)    
-      @event.add_food_truck(@food_truck2)    
+      @event.add_food_truck(@food_truck1)
+      @event.add_food_truck(@food_truck2)
       @event.add_food_truck(@food_truck3)
 
       expect(@event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+  end
+
+  describe '#food_trucks_that_sell' do
+    it 'can check for items sold' do
+      @food_truck1.stock(@item1, 35) 
+      @food_truck1.stock(@item2, 7)
+      @food_truck2.stock(@item4, 50)
+      @food_truck2.stock(@item3, 25)
+      @food_truck3.stock(@item1, 65) 
+
+      @event.add_food_truck(@food_truck1)
+      @event.add_food_truck(@food_truck2)
+      @event.add_food_truck(@food_truck3)
+
+      expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
+      expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
     end
   end
   
