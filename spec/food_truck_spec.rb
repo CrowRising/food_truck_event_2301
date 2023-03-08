56ad6f9d2d37b610bfc0fc0ab9679c031294ab89
Crow Rising <miscbyrd@gmail.com>
2023-03-08 09:29:34 -0700

    test: Add test to stock, check stock and inventory

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -29,10 +29,16 @@ RSpec.describe FoodTruck do
       expect(@food_truck.check_stock(@item1)).to eq(0)
     end
 
-    it 'stocks item' do
+    it 'stocks item and can check stock and inventory' do
       @food_truck.stock(@item1, 30)
       expect(@food_truck.inventory).to eq({ @item1 => 30 })
       expect(@food_truck.check_stock(@item1)).to eq(30)
+
+      @food_truck.stock(@item1, 25)
+      expect(@food_truck.check_stock(@item1)).to eq(55)
+
+      @food_truck.stock(@item2, 12)
+      expect(@food_truck.inventory).to eq({ @item1 => 55, @item2 => 12})
     end
   end
 end
