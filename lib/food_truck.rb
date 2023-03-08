f2856b3eae9db7bbe1ae691d68aba612246f91cd
Crow Rising <miscbyrd@gmail.com>
2023-03-08 09:25:53 -0700

    feat: Add methods for check stock and stock

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,9 +1,22 @@
 class FoodTruck
   attr_reader :name,
-              :inventory
+              :inventory, 
+              :check_stock
 
   def initialize(name)
     @name = name
     @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    check_stock = 0
+    @inventory.each do |key, value|
+      check_stock += value
+    end
+    check_stock
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
   end
 end
