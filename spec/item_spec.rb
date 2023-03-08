f9fb319032cb9bd9790be27a2524be63e0069ed8
Crow Rising <miscbyrd@gmail.com>
2023-03-08 09:05:15 -0700

    test: Iinitial commit

---
+++ spec/item_spec.rb
@@ -0,0 +1,9 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+end
