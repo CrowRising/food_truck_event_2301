f20d6b9745f8bcca7e6052a302dcd7c0146fdc59
Crow Rising <miscbyrd@gmail.com>
2023-03-08 11:28:11 -0700

    test: Fix syntax final commit

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe Event do
     @event = Event.new('South Pearl Street Farmers Market') 
     @food_truck1 = FoodTruck.new('Rocky Mountain Pies')
     @food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')  
-    @food_truck3 = FoodTruck.new('Palisade Peach Shack')  
-    @item1 = Item.new({ name: 'Peach Pie (Slice)', price: "$3.75" })
+    @food_truck3 = FoodTruck.new('Palisade Peach Shack')
+    @item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
     @item2 = Item.new({ name: 'Apple Pie (Slice)', price: '$2.50' })
-    @item3 = Item.new({ name: 'Peach-Raspberry Nice Cream', price: "$5.30" })
-    @item4 = Item.new({ name: 'Banana Nice Cream', price: "$4.25" })
+    @item3 = Item.new({ name: 'Peach-Raspberry Nice Cream', price: '$5.30' })
+    @item4 = Item.new({ name: 'Banana Nice Cream', price: '$4.25' })
   end
 
   describe '#intialize' do
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,7 +3,7 @@ require './lib/food_truck'
 
 RSpec.describe Item do
   before(:each) do
-    @item1 = Item.new({ name: 'Peach Pie (Slice)', price: "$3.75" })
+    @item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
     @item2 = Item.new({ name: 'Apple Pie (Slice)', price: '$2.50' })
   end
 
